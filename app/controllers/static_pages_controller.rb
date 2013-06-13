68e70f0b42697eefb4d46cf71a5f87f1869c554a
Joe Shoulak <JoepShoulak@me.com>
2013-06-12 22:53:26 -0500

    skeleton

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,22 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def bio
+  end
+
+  def music
+  end
+
+  def videos
+  end
+
+  def photos
+  end
+
+  def tour
+  end
+
+  def contact
+  end
+end
