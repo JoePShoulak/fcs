1cdb3cebd167daa4dfd3380dae33a34ca710ca06
Joe Shoulak <JoepShoulak@me.com>
2013-06-13 17:38:19 -0500

    Remove image with src of '#'

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,14 +8,13 @@ class StaticPagesController < ApplicationController
 
   def music
     itunes_prefix  = "https://itunes.apple.com/us/album/"
-    accident_url,  accident_src   = itunes_prefix + "accidental-love-letters-single/id485158289",  "#"
+    accident_url,  accident_src   = itunes_prefix + "accidental-love-letters-single/id485158289",  "#{}"
     forever_url,   forever_src    = itunes_prefix + "forever-and-tomorrow-single/id517796378",     "/assets/forever.jpg"
     green_url,     green_src      = itunes_prefix + "green-single/id510678840",                    "/assets/green.jpg"
     recluse_url,   recluse_src    = itunes_prefix + "the-recluse-ep/id579311062",                  "/assets/recluse.jpg"
     gone_url,      gone_src       = itunes_prefix + "while-i-was-gone/id486808582",                "/assets/gone.jpg"
-    @albums = [[accident_url, accident_src],  [forever_url, forever_src],
-                [green_url,   green_src],     [recluse_url, recluse_src],
-                [gone_url,    gone_src]]
+    @albums = [ [forever_url, forever_src], [green_url,   green_src],
+                [recluse_url, recluse_src], [gone_url,    gone_src]]
   end
 
   def videos
