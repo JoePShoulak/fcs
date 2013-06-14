e8ef1937b2f2e27365705b93137ab74e9e8779f2
Joe Shoulak <JoepShoulak@me.com>
2013-06-13 22:07:01 -0500

    Pictures are links to pictures

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,15 +8,27 @@ class StaticPagesController < ApplicationController
   end
 
   def music
-    itunes_prefix  = "https://itunes.apple.com/us/album/"
-    accident_url,  accident_src   = itunes_prefix + "accidental-love-letters-single/id485158289",  "accident.jpg"
-    forever_url,   forever_src    = itunes_prefix + "forever-and-tomorrow-single/id517796378",     "/assets/forever.jpg"
-    green_url,     green_src      = itunes_prefix + "green-single/id510678840",                    "/assets/green.jpg"
-    recluse_url,   recluse_src    = itunes_prefix + "the-recluse-ep/id579311062",                  "/assets/recluse.jpg"
-    gone_url,      gone_src       = itunes_prefix + "while-i-was-gone/id486808582",                "/assets/gone.jpg"
-    @albums = [ [accident_url,  accident_src],
-                [forever_url, forever_src], [green_url,   green_src],
-                [recluse_url, recluse_src], [gone_url,    gone_src]]
+    itunes_prefix     = "https://itunes.apple.com/us/album/"
+    accident_url      = itunes_prefix + "accidental-love-letters-single/id485158289"
+    forever_url       = itunes_prefix + "forever-and-tomorrow-single/id517796378"
+    green_url         = itunes_prefix + "green-single/id510678840"
+    recluse_url       = itunes_prefix + "the-recluse-ep/id579311062"
+    gone_url          = itunes_prefix + "while-i-was-gone/id486808582"
+    accident_src      = "/assets/accident.jpg"
+    forever_src       = "/assets/forever.jpg"
+    green_src         = "/assets/green.jpg"
+    recluse_src       = "/assets/recluse.jpg"
+    gone_src          = "/assets/gone.jpg"
+    accident_name     = "Accidental Love Letters"
+    forever_name      = "Forever and Tomorrow"
+    green_name        = "Green"
+    recluse_name      = "The Recluse"
+    gone_name         = "While I Was Gone"
+    @albums = [ [accident_url,  accident_src, accident_name ],
+                [forever_url,   forever_src,  forever_name  ],
+                [green_url,     green_src,    green_name    ],
+                [recluse_url,   recluse_src,  recluse_name  ],
+                [gone_url,      gone_src,     gone_name     ] ]
   end
 
   def videos
