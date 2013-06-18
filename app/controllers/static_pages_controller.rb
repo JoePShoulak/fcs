77759357251b86f5e918373cab5b1c853a8f7fec
Joe Shoulak <JoepShoulak@me.com>
2013-06-17 22:36:13 -0500

    Add tile photo

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -45,7 +45,8 @@ class StaticPagesController < ApplicationController
                 ["/assets/mpls.jpg",      "/assets/mpls-large.jpg"],
                 ["/assets/singing.jpg",   "/assets/singing-large.jpg"],
                 ["/assets/smile.jpg",     "/assets/smile-large.jpg"],
-                ["/assets/smile2.jpg",    "/assets/smile2-large.jpg"] ]
+                ["/assets/smile2.jpg",    "/assets/smile2-large.jpg"],
+                ["/assets/terrarium.jpg", "/assets/terrarium-large.jpg"] ]
   end
 
   def tour
